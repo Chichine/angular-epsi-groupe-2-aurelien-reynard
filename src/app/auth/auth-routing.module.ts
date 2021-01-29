d092d77fbe3fdf1f213a68aba1ae6406b66f6c70
Chichine <chichinedev@gmail.com>
2021-01-29 13:24:40 +0100

    Initial commit

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {SignupComponent} from './components/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'signin', component: SigninComponent,
+  },
+  {
+    path: 'signup', component: SignupComponent,
+  },
+  {
+    path: 'home', component: HomeComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
